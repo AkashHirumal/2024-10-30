feee1922592ba7249538b55e12a9f036a44d83e1
AkashHirumal <akashhirumal@gmail.com>
2024-10-30 11:18:33 +0530

    3rd commit

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const guessNumber = Math.floor((Math.random()*10)+1);
     console.log(guessNumber);
-    let countNum = 0;
+    let countNum = 3;
     
 
     
     function myGuess(){
-    let guessText = Number(document.getElementById("txtGuess").value);  
+    if(countNum>0){
+        let guessText = Number(document.getElementById("txtGuess").value);  
    
         while(guessText!==guessNumber){
             // console.log("yes");
             resalt.innerHTML = "TRY";
-            countNum = countNum + 1;
-            countc.innerHTML = 3-countNum;
+            countNum = countNum - 1;
+            countc.innerHTML = countNum;
+            console.log()
             break;
         }
         if(guessText==guessNumber){
             resalt.innerHTML = "Congragulation Your WIN";
+            
         }
-        if(countNum == 3){
-            resalt.innerHTML = "Game Over! <br> The Correct Number was " + guessNumber; 
+        if(countNum == 0){
+            resalt.innerHTML = "Game Over! <br> The Correct Number was " + guessNumber;
+             
         }
+       
     }
 
-  
+}
 
